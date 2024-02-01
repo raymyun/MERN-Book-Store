e61b30e8ba900dc1f2ba0bf16d388391553de158
Raymond Yang <yangraymond2011@gmail.com>
2024-01-31 18:58:54 -0800

    Route to Get All Books
    Testing around with multiple book objects

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,11 @@ app.get('/', (request, response) => {
     return response.status(234).send('Get / Test with Response Status of 234');
 });
 
-// Route - Adding a new book
+// Route - Adding a new book to the database
 app.post('/books', async (request, response) => {
     try {
         if (!request.body.title || !request.body.author || !request.body.publishYear) {
+            // Status 400 for client error
             return response.status(400).send({ message: 'Provide all required fields: title, author, publishYear'});
         }
         const newBook = {
@@ -27,11 +28,28 @@ app.post('/books', async (request, response) => {
             publishYear: request.body.publishYear
         };
         const book = await Book.create(newBook);
+        // Status 201 for creation
         return response.status(201).send(book);
 
     } catch (error) {
         console.log(error.message);
+        // Status 500 for server error
         response.status(500).send({ message: error.message })
+    }
+});
+
+// Route - Get all books from the database
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({})
+        // Status 200 for success
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
     }
 });
 
