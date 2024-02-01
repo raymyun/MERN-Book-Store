64093dc12ac2e9cee27069e68de3704d8efc4247
Raymond Yang <yangraymond2011@gmail.com>
2024-01-31 17:59:24 -0800

    Get resource from server test using / route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Get / Test with Response Status of 234')
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`)
 });
