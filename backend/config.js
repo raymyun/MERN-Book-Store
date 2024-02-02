e57d5fdcf015d11178f2206228608335b948a4d1
Raymond Yang <yangraymond2011@gmail.com>
2024-02-01 20:45:15 -0800

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,5 @@
 export const PORT = 5555;
 
+// Need to make this mondoDBURL non-accessable by public
+// Probably use .env
 export const mongoDBURL = 'mongodb+srv://bookstoreADMIN:ADMINraymo@mern-book-store.rtortvf.mongodb.net/book-collection?retryWrites=true&w=majority';
