04c1b218a69f4d4c9beafec7f27c9f0e5b26a310
Raymond Yang <yangraymond2011@gmail.com>
2024-02-07 20:20:33 -0800

    More style tinkering

--- frontend/src/components/home/BooksCards.jsx
+++ frontend/src/components/home/BooksCards.jsx
@@ -13,24 +13,24 @@ const BooksCards = ({ books }) => {
       {books.map((item) => (
         <div
           key={item._id}
-          className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'
+          className='border-2 border-gray-500 bg-blue-300 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'
         >
-          <h2 className='absolute top-1 right-2 px-4 py-1 bg-blue-400 text-black rounded-lg'>
+          <h2 className='absolute top-1 right-2 px-4 py-1 bg-gray-300 text-black rounded-lg'>
             {item.publishYear}
           </h2>
-          <h4 className='my-2 text-gray-500'>
-            <span className='font-semibold'>Book ID: </span> 
+          <h4 className='my-2 text-gray-800'>
+            <span className=''>Book ID: </span> 
             {item._id}
           </h4>
           <div className='flex justify-start items-center gap-x-2'>
             <PiBookOpenTextLight className='text-blue-500 text-2xl' />
-            <h2 className='my-1'>
+            <h2 className='font-semibold my-1'>
               {item.title}
             </h2>
           </div>
           <div className='flex justify-start items-center gap-x-2'>
             <BiUserCircle className='text-blue-500 text-2xl' />
-            <h2 className='my-1'>
+            <h2 className='font-semibold my-1'>
               {item.author}
             </h2>
           </div>
