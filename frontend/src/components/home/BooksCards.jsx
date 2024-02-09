a89aeb60482813a89953cec30ed651af349f0b93
Raymond Yang <yangraymond2011@gmail.com>
2024-02-08 20:30:44 -0800

    Refactor BooksSingleCard Display

--- frontend/src/components/home/BooksCards.jsx
+++ frontend/src/components/home/BooksCards.jsx
@@ -5,47 +5,14 @@ import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineDelete } from 'react-icons/md';
 import { PiBookOpenTextLight } from 'react-icons/pi';
 import { BiUserCircle } from 'react-icons/bi';
+import BooksSingleCard from './booksSingleCard';
 
 
 const BooksCards = ({ books }) => {
   return (
     <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
       {books.map((item) => (
-        <div
-          key={item._id}
-          className='border-2 border-gray-500 bg-blue-300 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'
-        >
-          <h2 className='absolute top-1 right-2 px-4 py-1 bg-gray-300 text-black rounded-lg'>
-            {item.publishYear}
-          </h2>
-          <h4 className='my-2 text-gray-800'>
-            <span className=''>Book ID: </span> 
-            {item._id}
-          </h4>
-          <div className='flex justify-start items-center gap-x-2'>
-            <PiBookOpenTextLight className='text-blue-500 text-2xl' />
-            <h2 className='font-semibold my-1'>
-              {item.title}
-            </h2>
-          </div>
-          <div className='flex justify-start items-center gap-x-2'>
-            <BiUserCircle className='text-blue-500 text-2xl' />
-            <h2 className='font-semibold my-1'>
-              {item.author}
-            </h2>
-          </div>
-          <div className='flex justify-between items-center gap-x-2 mt-4 p-4'>
-            <Link to={`/books/details/${item._id}`}>
-              <BsInfoCircle className='text-2xl text-green-800 hover:text-black' />
-            </Link>
-            <Link to={`/books/edit/${item._id}`}>
-              <AiOutlineEdit className='text-2xl text-yellow-600 hover:text-black' />
-            </Link>
-            <Link to={`/books/delete/${item._id}`}>
-              <MdOutlineDelete className='text-2xl text-red-600 hover:text-black' />
-            </Link>
-          </div>
-        </div>
+        <BooksSingleCard key={item._id} book={item} />
       ))}
     </div>
   )
---
+++ frontend/src/components/home/BooksSingleCard.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineDelete } from 'react-icons/md';
+import { PiBookOpenTextLight } from 'react-icons/pi';
+import { BiUserCircle } from 'react-icons/bi';
+
+const BooksSingleCard = ({book}) => {
+    return (
+        <div
+            key={book._id}
+            className='border-2 border-gray-500 bg-blue-300 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'
+        >
+            <h2 className='absolute top-1 right-2 px-4 py-1 bg-gray-300 text-black rounded-lg'>
+                {book.publishYear}
+            </h2>
+            <h4 className='my-2 text-gray-800'>
+                <span className=''>Book ID: </span>
+                {book._id}
+            </h4>
+            <div className='flex justify-start items-center gap-x-2'>
+                <PiBookOpenTextLight className='text-blue-500 text-2xl' />
+                <h2 className='font-semibold my-1'>
+                    {book.title}
+                </h2>
+            </div>
+            <div className='flex justify-start items-center gap-x-2'>
+                <BiUserCircle className='text-blue-500 text-2xl' />
+                <h2 className='font-semibold my-1'>
+                    {book.author}
+                </h2>
+            </div>
+            <div className='flex justify-between items-center gap-x-2 mt-4 p-4'>
+                <Link to={`/books/details/${book._id}`}>
+                    <BsInfoCircle className='text-2xl text-green-800 hover:text-black' />
+                </Link>
+                <Link to={`/books/edit/${book._id}`}>
+                    <AiOutlineEdit className='text-2xl text-yellow-600 hover:text-black' />
+                </Link>
+                <Link to={`/books/delete/${book._id}`}>
+                    <MdOutlineDelete className='text-2xl text-red-600 hover:text-black' />
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default BooksSingleCard
